bac1972a414ff08d8504af06bf7e49e275aa1fde
robin mahmud <robinmahmud358@gmail.com>
2022-12-27 15:37:55 +0600

    data uploaded server

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { authContext } from '../../AuthProvider/AuthProvider';
 
 const About = () => {
+    const { user } = useContext(authContext)
+    console.log(user)
     return (
         <div>
             <h2>About</h2>
--- src/Share/Login/SignUp.js
+++ src/Share/Login/SignUp.js
@@ -1,3 +1,4 @@
+
 import React, { useContext, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { Link, useNavigate } from 'react-router-dom';
@@ -14,13 +15,27 @@ const SignUp = () => {
         createUser(data.email, data.password)
             .then(result => {
                 const user = result.user;
-                console.log(user)
+                handleUserInfo(data.name, data.email, data.university, data.address)
                 navigate('/login')
                 toast.success('user Create Successfully');
             })
             .catch(error => {
                 console.log(error)
                 setSignupError(error.message)
+            })
+    }
+    const handleUserInfo = (name, email, university, address) => {
+        const userInfo = { name, email, university, address }
+        fetch(`http://localhost:5000/userInfo`, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(userInfo)
+        })
+            .then(res => res.json())
+            .then(data => {
+                toast.success('User Data Uploaded');
             })
     }
     return (
@@ -38,6 +53,7 @@ const SignUp = () => {
                         })} className="input input-bordered w-full max-w-xs" />
                         {errors.name && <p className='text-red-600'>{errors.name?.message}</p>}
                     </div>
+
                     <div className="form-control w-full max-w-xs">
                         <label className="label">
                             <span className="label-text">Email</span>
@@ -46,6 +62,24 @@ const SignUp = () => {
                             required: "Email Address is required"
                         })} className="input input-bordered w-full max-w-xs" />
                         {errors.email && <p className='text-red-600'>{errors.email?.message}</p>}
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text">University</span>
+                        </label>
+                        <input type="text" {...register("university", {
+                            required: "university is required"
+                        })} className="input input-bordered w-full max-w-xs" />
+                        {errors.university && <p className='text-red-600'>{errors.university?.message}</p>}
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text">Address</span>
+                        </label>
+                        <input type="text" {...register("address", {
+                            required: "Address is required"
+                        })} className="input input-bordered w-full max-w-xs" />
+                        {errors.address && <p className='text-red-600'>{errors.address?.message}</p>}
                     </div>
                     <div className="form-control w-full max-w-xs">
                         <label className="label">
@@ -65,8 +99,7 @@ const SignUp = () => {
 
                 }
                 <p>Already have an Account?<Link className='text-secondary' to='/login'>Please Login</Link></p>
-                <div className="divider">OR</div>
-                <button className='btn btn-outline w-full'>COUNTINUE WITH GOOGLE</button>
+
 
             </div>
         </div>
